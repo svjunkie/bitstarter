dfafea2df71d52ae6c2b382e4756e307d9f75b01
John Dryden <john.c.dryden@gmail.com>
2013-07-11 21:14:47 +0000

    add a few lines to grader.js

--- grader.js
+++ grader.js
@@ -4,4 +4,13 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var HTMLFILE_DEFAULT = "index.html";
+var CHECKSFILE_DEFAULT = "checks.json";
 
+
+var assertFileExists = funciton(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+    }
+    return instr;
+};
