d84351e06b33c0df4bd3ad191f2c9f667b812314
John Dryden <john.c.dryden@gmail.com>
2013-07-11 01:43:14 +0000

    Fixed web.js with quotation marks

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buf = new Buffer(fs.readFileSync(index.html));
+  var buf = new Buffer(fs.readFileSync('index.html'));
   response.send(buf.toString());
 });
 
