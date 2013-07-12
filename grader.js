6ace86a0a53050cda89fd00d410063978d918b34
John Dryden <john.c.dryden@gmail.com>
2013-07-12 06:00:16 +0000

    fixed grader.js v1

--- grader.js
+++ grader.js
@@ -3,12 +3,13 @@
 
 var fs = require('fs');
 var program = require('commander');
+var rest = require('restler');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 
-var assertFileExists = funciton(infile) {
+var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
 	console.log("%s does not exist. Exiting.", instr);
@@ -28,7 +29,7 @@ var loadChecks = function(checksfile) {
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
-    var out {};
+    var out = {};
     for(var ii in checks) {
 	var present = $(checks[ii]).length > 0;
 	out[checks[ii]] = present;
@@ -42,13 +43,15 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-if(require.main == module {
+if(require.main == module) {
     program
        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+//       .option('-u, --url <html_url>', 'URL to index.html',clone(assertUrlExists))
        .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
+//    rest.get(program.url).on('complete',
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
